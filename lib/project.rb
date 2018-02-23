2f10545868d1f6c6a5dc290aab0a6ef5db1fa003
Misha Petrov <modesau@gmail.com>
2018-02-23 12:47:13 -0800

    pass .all test for class Project

--- lib/project.rb
+++ lib/project.rb
@@ -13,4 +13,15 @@ class Project
     self.title().==(another_project.title())
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects_list = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects_list.push(Project.new({:title => title, :id => id}))
+    end
+    projects_list
+  end
+
 end
