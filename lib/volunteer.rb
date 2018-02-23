de8b3061100f226012e6555a5179f59bbc03dc6f
Misha Petrov <modesau@gmail.com>
2018-02-23 13:13:25 -0800

    pass #save and .all tests for class Volunteer

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -18,11 +18,16 @@ class Volunteer
     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
     volunteers_list = []
     returned_volunteers.each() do |volunteer|
-      name = volunteer.fetch("title")
+      name = volunteer.fetch("name")
       project_id = volunteer.fetch("project_id").to_i
       id = volunteer.fetch("id").to_i
-      volunteers_list.push(Volunteer.new({:name => title, :project_id => project_id, :id => id}))
+      volunteers_list.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
     volunteers_list
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
   end
 end
