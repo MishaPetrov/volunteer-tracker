25ff963faf1ed1646c6676543e34002952a46d37
Misha Petrov <modesau@gmail.com>
2018-02-23 13:11:07 -0800

    add #save test for class Volunteer

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -13,4 +13,16 @@ class Volunteer
   def ==(another_volunteer)
     self.name().==(another_volunteer.name())
   end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers_list = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("title")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers_list.push(Volunteer.new({:name => title, :project_id => project_id, :id => id}))
+    end
+    volunteers_list
+  end
 end
