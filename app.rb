8cba2995416cbb41a090690b337c7964635c2d58
Misha Petrov <modesau@gmail.com>
2018-02-23 13:48:22 -0800

    pass first integration spec test

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**.*.rb')
+require('./lib/project')
+require('./lib/volunteer')
 require('pry')
+require('pg')
+
+DB = PG.connect({:dbname => "volunteer_tracker_test"})
 
 get('/') do
-  erb(:input)
+  @projects_list = Project.all()
+  erb(:index)
 end
 
-get('/output') do
-  erb(:output)
+post('/') do
+  title = params.fetch("title")
+  project = Project.new(:title => title, :id => nil)
+  project.save
+  @projects_list = Project.all()
+  erb(:index)
 end
